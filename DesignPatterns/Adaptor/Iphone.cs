0a2e80e946699ed0bbc98007590f5f7bc43b4fe0
Eugene Kagan <eugene@Eugenes-MacBook-Pro.local>
2019-12-20 14:18:33 -0500

    init

---
+++ DesignPatterns/Adaptor/Iphone.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace DesignPatterns.Adaptor
+{
+    // code that we already have
+    public class AppleEarBuds
+    {
+        public void AuxInput(String song)
+        {
+            Console.WriteLine("****************************************");
+            Console.WriteLine(String.Format("****** {0} ******", song));
+            Console.WriteLine("****************************************");
+        }
+    }
+
+    public interface IOutput
+    {
+        void Input(String song);
+    }
+
+    public class HeadphonesAdaptor : IOutput
+    {
+        AppleEarBuds headphones = new AppleEarBuds();
+
+        public void Input(String song)
+        {
+            headphones.AuxInput(song);
+        }
+    }
+
+    // let's say we are given a pre-writem class that we cannot change the interface of.
+    class IphoneX
+    {
+        private IOutput _output;
+
+        public void setConverter(IOutput headphones)
+        {
+            this._output = headphones;
+        }
+
+        // there are 1000 dependencies on this method
+        public void playSong(String song)
+        {
+            this._output.Input(song);
+        }
+    }
+
+}
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DesignPatterns.Adaptor;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IphoneX iphoneX = new IphoneX();
+            HeadphonesAdaptor adoptor = new HeadphonesAdaptor();
+            iphoneX.setConverter(adoptor);
+            iphoneX.playSong("Mobb Deep - Win or Lose");
+        }
+    }
+}
